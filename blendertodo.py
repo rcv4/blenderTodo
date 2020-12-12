a9924d459a3188bba4e0d365c5e7400776eff3c4
rcv4 <68054150+rcv4@users.noreply.github.com>
2020-12-12 13:17:55 +0100

    Enable "Show in N-Panel" functionality

--- blendertodo.py
+++ blendertodo.py
@@ -59,17 +59,14 @@ class Settings(bpy.types.AddonPreferences):
 # UI
 #
 
-class TodoPanel(bpy.types.Panel):
-    bl_idname = "BLENDERTODO_PT_ToDo"
-    bl_label = "Blender Todo"
-    bl_space_type = "VIEW_3D"
+class TodoPanel:
     bl_region_type = "UI"
-    bl_category = "To-Do"
+    bl_space_type = "VIEW_3D" 
 
     def clamp(self, n):
         return max(min(20, n), 3)
 
-    def draw(self, context):
+    def draw_base(self,context):
         l = self.layout
         loc = context.scene.bl_todo
         l.template_list("TODO_UL_ToDoList", "", loc, "todo_list", loc, "index", rows=self.clamp(len(loc.todo_list) + 1), type="DEFAULT")
@@ -79,16 +76,58 @@ class TodoPanel(bpy.types.Panel):
         r.label(text="Move to")
         r.operator("blendertodo.top", text="Top", icon="TRIA_UP_BAR")
         r.operator("blendertodo.bottom", text="Bottom", icon="TRIA_DOWN_BAR")
-        l.separator()
+
+
+
+#Calling directly causes "[..]\blender.git\source\blender\python\intern\bpy_rna.c:8341 bpy_class_call: unable to get Python class for RNA struct 'BLENDERTODO_PT_ToDoBase'"
+class TodoIn3DPanel(TodoPanel, bpy.types.Panel):
+    bl_idname = "BLENDERTODO_PT_3DView"
+    bl_label = "Blender Todo"
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def draw(self, context):
+        self.draw_base(context)
+
+        l = self.layout
+        loc = context.scene.bl_todo
+
+        #You cant double-click to edit in popovers (https://developer.blender.org/T66286)
+        selected = loc.todo_list[loc.index]
+        c = l.column(align=True)
+        c.label(text="Edit: ")
+        b = c.box()
+        r = b.row()
+        r.prop(selected, "done", text="")
+        r.prop(selected, "content", text="")
+
+
+
+class TodoInNPanel(TodoPanel, bpy.types.Panel):
+    bl_idname = "BLENDERTODO_PT_NPanel"
+    bl_category = "To-Do"
+    bl_label = "Blender Todo"
+
+    @classmethod
+    def poll(cls, context):
+        return context.preferences.addons[__name__.partition('.')[0]].preferences.n_panel_enabled
+
+    def draw(self, context):
+        self.draw_base(context)
+        self.layout.separator()
         
+
+
 
 def view_panel_callback(self, context):
     prefs = context.preferences.addons[__name__.partition('.')[0]].preferences
     if prefs.popover_enabled:
         if prefs.popover_display_as == "icon":
-            self.layout.popover(TodoPanel.bl_idname, text="", icon="PRESET")
+            self.layout.popover(TodoIn3DPanel.bl_idname, text="", icon="PRESET")
         else:
-            self.layout.popover(TodoPanel.bl_idname, text="To-Do")
+            self.layout.popover(TodoIn3DPanel.bl_idname, text="To-Do")
         
     
 
@@ -121,7 +160,7 @@ class TodoList(bpy.types.UIList):
 
 
 
-bl_classes=[TodoItem, TodoList, SceneSave, Settings,TodoPanel]
+bl_classes=[TodoItem, TodoList, SceneSave, Settings, TodoInNPanel, TodoIn3DPanel]
 
 #
 # OPS
